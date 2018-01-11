0401f01184750d58400717be3d1329a417283cab
Alessandro Di Maria <adm@m42.ch>
2018-01-11 10:42:44 +0100

    route repors/new

---
+++ app/javascript/pages/ReportFormPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const ReportFormPage = () => (
+  <div>
+    <h1>New Report</h1>
+    <form>
+      <label>Pferde Name:</label>
+      <input type="text" name="horse_name" value="Impi" />
+    </form>
+  </div>
+)
+
+export default ReportFormPage
--- app/javascript/store/routes.js
+++ app/javascript/store/routes.js
@@ -1,14 +1,26 @@
 import { redirect } from 'redux-first-router'
 import { flipFoo } from './modules/foo'
 import FooPage from 'pages/Foo'
+import ReportFormPage from 'pages/ReportFormPage'
 
 export const ROOT = 'ROOT'
+export const REPORTS = 'REPORTS'
+export const NEW_REPORT = 'NEW_REPORT'
 export const FOO = 'FOO'
 
 const routesMap = {
   [ROOT]: {
     path: '/',
     thunk: (dispatch, _getState) => dispatch(redirect({ type: FOO }))
+  },
+
+  [REPORTS]: {
+    path: '/reports'
+  },
+
+  [NEW_REPORT]: {
+    path: '/reports/new',
+    page: ReportFormPage
   },
 
   [FOO]: {
